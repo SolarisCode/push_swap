2b93c5237403abef538d6623670b28b53f724ef1
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2022-10-16 00:37:20 +0200

    First error handling functions

--- main_errorhandling.c
+++ main_errorhandling.c
@@ -1,113 +1,105 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   string_and_char_bonus.c                            :+:      :+:    :+:   */
+/*   main_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/25 09:47:33 by melkholy          #+#    #+#             */
-/*   Updated: 2022/08/27 18:17:26 by melkholy         ###   ########.fr       */
+/*   Created: 2022/10/15 18:49:04 by melkholy          #+#    #+#             */
+/*   Updated: 2022/10/16 00:00:10 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf_bonus.h"
-#include "libft/libft.h"
+#include <stdlib.h>
+#include "ft_printf/ft_printf_bonus.h"
+#include "ft_printf/libft/libft.h"
+#include <stdbool.h>
 
-void	ft_format_s(t_print *buff, char *str, int len)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	buff->count = -1;
-	if (buff->flag == '-')
-	{
-		if (buff->s_pres)
-			while (++buff->count < buff->pres)
-				buff->str[buff->count] = str[buff->count];
-		else
-		{
-			while (str[++buff->count])
-				buff->str[buff->count] = str[buff->count];
-		}
-	}
-	else
-	{
-		buff->count = ft_strlen(buff->str);
-		if (buff->s_pres)
-			while (--buff->pres >= 0)
-				buff->str[--buff->count] = str[buff->pres];
-		else
-			while (--len >= 0)
-				buff->str[--buff->count] = str[len];
-	}
+	size_t	count;
+
+	count = 0;
+	while (s1[count] && s2[count] && s1[count] == s2[count])
+		count ++;
+	return ((unsigned char)s1[count] - (unsigned char)s2[count]);
 }
 
-void	ft_width_and_s(t_print *buff, char *str, int len)
+bool	ft_check_nbr(char **argv)
 {
-	if ((buff->pres > buff->width && buff->pres > len) || buff->pres > len)
-		buff->pres = len;
-	ft_stralloc(buff, NULL);
-	ft_format_s(buff, str, len);
-	buff->pchr += write(1, buff->str, ft_strlen(buff->str));
-	free(buff->str);
-}
+	int	count;
+	int	index;
 
-void	ft_pres_and_s(t_print *buff, char *str, int len)
-{
-	if (buff->s_pres && buff->pres < len)
-	{
-		if (buff->pres == 0)
-			return ;
-		ft_stralloc(buff, NULL);
-		buff->count = -1;
-		while (++buff->count < buff->pres)
-			buff->str[buff->count] = str[buff->count];
-		buff->pchr += write(1, buff->str, ft_strlen(buff->str));
-		free(buff->str);
-	}
-	else
-		buff->pchr += write(1, str, len);
-}
-
-void	ft_character(t_print *buff, va_list arg)
-{
-	char	c;
-	int		count;
-
-	c = va_arg(arg, int);
 	count = -1;
-	if (!buff->width)
+	while (argv[++count])
 	{
-		write(1, &c, 1);
-		buff->pchr ++;
+		index = -1;
+		if (argv[count][0] == '-')
+			index ++;
+		while (argv[count][++index])
+			if (!ft_isdigit(argv[count][index]))
+				return (ft_putstr_fd("Error\n", 1), false);
+		index = -1;
+		while (argv[++index])
+			if (count != index && !ft_strcmp(argv[count], argv[index]))
+					return (ft_putstr_fd("Error\n", 1), false);
 	}
-	else
-	{
-		buff->str = (char *)ft_calloc(buff->width + 1, sizeof(char));
-		while (++count < buff->width)
-			buff->str[count] = ' ';
-		if (buff->flag == '-')
-			buff->str[0] = c;
-		else
-			buff->str[ft_strlen(buff->str) - 1] = c;
-		write(1, buff->str, buff->width);
-		buff->pchr += buff->width;
-		free(buff->str);
-	}
+	return (true);
 }
 
-void	ft_string(t_print *buff, char *str)
+bool	ft_check_max(char **argv, int *array)
 {
-	int		len;
+	int		count;
+	bool	max;
 
-	buff->speci = 's';
-	if (!str)
+	count = -1;
+	max = false;
+	while (argv[++count])
 	{
-		str = ft_strdup("(null)");
-		buff->s_null ++;
+		if (argv[count][0] == '-' && array[count] > 0)
+			max = true;
+		else if (argv[count][0] != '-' && array[count] < 0)
+			max = true;
 	}
-	len = ft_strlen(str);
-	if (buff->width > len || (buff->width && buff->s_pres))
-		ft_width_and_s(buff, str, len);
+	free(array);
+	if (max)
+		ft_putstr_fd("Error\n", 1);
+	return (max);
+}
+
+bool	ft_sorted_max(char **argv)
+{
+	int	size;
+	int	count;
+	int	*array;
+
+	size = -1;
+	while (argv[++size]);
+	array = (int *)ft_calloc(size, sizeof(int));
+	if (!array)
+		return (NULL);
+	count = -1;
+	while (argv[++count])
+		array[count] = ft_atoi(argv[count]);
+	count = 0;
+	while(count <= size - 2 && array[count] < array[count + 1])
+		count ++;
+	if (count == size - 1)
+		return (free(array), true);
+	return (ft_check_max(argv, array));
+}
+
+void	ft_check_input(char **argv)
+{
+	if (!ft_check_nbr(argv) || ft_sorted_max(argv))
+		return ;
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 2)
+		return (1);
 	else
-		ft_pres_and_s(buff, str, len);
-	if (buff->s_null)
-		free(str);
+		ft_check_input(&argv[1]);
+	return (0);
 }
